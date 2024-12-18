5369739bd26e1323e098885f5e7e0d323ed475fd
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-12-18 15:46:27 +0300

    изменение визуала окна куар кода

--- src/features/qr-generator/ui/QRCodePreview/QRCodePreview.tsx
+++ src/features/qr-generator/ui/QRCodePreview/QRCodePreview.tsx
@@ -4,7 +4,6 @@ import { QRCode } from '@jackybaby/react-custom-qrcode'
 import { Button } from "@/shared/ui/button"
 import { Card } from "@/shared/ui/card"
 import { useDownloadQR } from '@/shared/lib/useDownloadQr'
-import { useState } from 'react'
 
 interface QRCodePreviewProps {
   value: string
@@ -17,34 +16,32 @@ export function QRCodePreview({ value, logoUrl = "/main-logo.svg", onEdit }: QRC
   const qrCodeId = "qr-code-canvas"
 
   return (
-    <Card className="flex-1">
-      <div className="p-6 space-y-6">
-        <div className="aspect-square relative">
-          <QRCode
-            value={value}
-            size={256}
-            ecLevel="H"
-            quietZone={0}
-            id={qrCodeId}
-            qrStyle="dots"
-            logoImage={logoUrl}
-            logoWidth={56}
-            logoHeight={40}
-            removeQrCodeBehindLogo={true}
-            eyeRadius={1080}
-            eyeColor={['#000000', '#000000', '#000000']}
-            bgColor="#FFFFFF"
-            fgColor="#000000"
-            style={{ width: '100%', height: '100%' }}
-          />
-        </div>
-        <Button 
-          onClick={onEdit} 
-          className="w-full"
-        >
-          Редактировать
-        </Button>
+    <Card className="w-full h-screen flex flex-col justify-center items-center p-4">
+      <div className="max-w-[512px] w-full aspect-square relative mb-6">
+        <QRCode
+          value={value}
+          size={512}
+          ecLevel="H"
+          quietZone={0}
+          id={qrCodeId}
+          qrStyle="dots"
+          logoImage={logoUrl}
+          logoWidth={56}
+          logoHeight={40}
+          removeQrCodeBehindLogo={true}
+          eyeRadius={1080}
+          eyeColor={['#000000', '#000000', '#000000']}
+          bgColor="#FFFFFF"
+          fgColor="#000000"
+          style={{ width: '100%', height: '100%' }}
+        />
       </div>
+      <Button 
+        onClick={onEdit} 
+        className="w-full max-w-[512px]"
+      >
+        Редактировать
+      </Button>
     </Card>
   )
 } 
