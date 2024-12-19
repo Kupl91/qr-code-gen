4b0629ca8c9a8affc981262fd768b054a69a5136
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-12-19 19:58:22 +0300

    +webVitals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import { QRGeneratorWidget } from "@/widgets/qr-generator"
+// src/app/page.tsx
+import { QRGeneratorWidget } from "@/widgets/qr-generator";
+import reportWebVitals from '../reportWebVitals';
 
 export default function Page() {
   return (
     <main className="min-h-screen flex items-center justify-center">
       <QRGeneratorWidget />
     </main>
-  )
-} 
+  );
+}
+
+// Передача функции для обработки метрик
+reportWebVitals(console.log); // Вывод метрик в консоль
