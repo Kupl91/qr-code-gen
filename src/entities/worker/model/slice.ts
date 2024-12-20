b36cf2f8924ca93730b7c498dacf690181bc54a6
Kupl91 <125669838+Kupl91@users.noreply.github.com>
2024-12-20 14:03:08 +0300

    fix

--- src/entities/worker/model/slice.ts
+++ src/entities/worker/model/slice.ts
@@ -4,10 +4,14 @@ import type { WorkerDTO } from '../api/types'
 
 interface WorkerState {
   data: WorkerDTO | null
+  loading: boolean
+  error: string | null
 }
 
 const initialState: WorkerState = {
-  data: null
+  data: null,
+  loading: false,
+  error: null
 }
 
 export const workerSlice = createSlice({
@@ -16,6 +20,14 @@ export const workerSlice = createSlice({
   reducers: {
     setWorkerData: (state, action: PayloadAction<WorkerDTO>) => {
       state.data = action.payload
+      state.error = null
+    },
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload
+    },
+    setError: (state, action: PayloadAction<string>) => {
+      state.error = action.payload
+      state.loading = false
     }
   }
 })
